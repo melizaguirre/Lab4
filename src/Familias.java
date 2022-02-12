a75cd0d2e002e7ef40660843567afdd272e65b0b
melizaguirre <BAC@MacBook-Air-de-Mac.local>
2022-02-11 18:01:43 -0600

    Familia

--- src/Familias.java
+++ src/Familias.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +12,20 @@
  * @author BAC
  */
 public class Familias {
+    
+       ArrayList<Aldeanos> familia = new ArrayList();
     private String apellido;
+
+    public Familias(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
     
 }
---
+++ src/Practica.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BAC
+ */
+public class Practica {
+    public static void main(String[] args){
+        
+        Aldeanos herrero = new Herrero("Pedro", "Lopez", 45, 5,"Cabo");
+        Aldeanos agronomo = new  Agronomo("Juancito", "Cruz", 50, 6, "Sargento");
+        
+    }
+    public static void defaultAni(){
+        familia.add(New Aldeanos("Montesco"));
+    }
+}
